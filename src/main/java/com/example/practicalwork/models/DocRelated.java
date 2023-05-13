7842fc84bf12e373cd50a147a26868cb4183cf0e
svbabaya <babayan.serge@gmail.com>
2023-05-13 16:43:09 +0300

    Add swagger in pom, list of related docs

---
+++ src/main/java/com/example/practicalwork/models/DocRelated.java
@@ -0,0 +1,23 @@
+package com.example.practicalwork.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class DocRelated implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long relatedId;
+
+}
