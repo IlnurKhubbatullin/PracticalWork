ba9c780bccc894e6eccea43d0f9bd22ece380f91
svbabaya <babayan.serge@gmail.com>
2023-05-20 14:37:13 +0300

    Add DocumentController and getAllDocs()

--- src/main/java/com/example/practicalwork/convertors/DocumentConvertor.java
+++ src/main/java/com/example/practicalwork/convertors/DocumentConvertor.java
@@ -2,15 +2,19 @@ package com.example.practicalwork.convertors;
 
 import com.example.practicalwork.DTO.DocumentDTO;
 import com.example.practicalwork.models.Document;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class DocumentConvertor {
 
     private ModelMapper modelMapper;
 
     public DocumentDTO convertToDto(Document entity){
+
         return modelMapper.map(entity, DocumentDTO.class);
     }
 }
