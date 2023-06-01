c2f35a1da7767238662a7f41cca0346a6b314841
svbabaya <babayan.serge@gmail.com>
2023-06-01 11:20:19 +0300

    Add 3 fields in V2__...sql

---
+++ src/main/resources/db.migration/V2__Insert_data_documents_templates_fields.sql
@@ -0,0 +1,22 @@
+insert into schema_system.field (created_at, default_value,
+        name, placeholder,
+        removed, type, updated_at, document_id, template_id)
+values ('2023-24-01 12:40:40', 'Limited Liability Company, hereinafter referred to as the Customer, represented by the General Director, acting on the basis of the charter, on the one hand, and the Individual Entrepreneur, hereinafter referred to as the Contractor, acting on the basis of a certificate of state registration of an individual as an individual entrepreneur number 445673847588493, on the other The parties have entered into this agreement as follows:',
+        'Header of contract', 'Information about the customer and contractor',
+        false, 'Contract field', '2023-24-01 13:25:07', 1, 1);
+
+insert into schema_system.field (created_at, default_value,
+        name, placeholder,
+        removed, type, updated_at, document_id, template_id)
+values ('2023-24-01 12:45:15', 'Subject of the contract. The Customer instructs, and the Contractor assumes the obligation to provide the services specified in the Annexes to this Agreement. The customer undertakes to accept and pay for these services.
+The period for the provision of services provided for by this Agreement is determined by the Annexes to this Agreement (hereinafter referred to as the "Period for the provision of Services").
+To carry out this activity, the Contractor uses its own staff, and, if necessary, hires additional staff or engages third parties for cooperation, while remaining responsible for their actions.',
+        'Body of contract', 'Sections of contract: conditions, description of work, terms of payment and so on',
+        false, 'Contract field', '2023-24-01 13:42:32', 1, 1);
+
+insert into schema_system.field (created_at, default_value,
+        name, placeholder,
+        removed, type, updated_at, document_id, template_id)
+values ('2023-24-01 15:23:41', 'Other conditions. When providing services in accordance with this Agreement, each of the Parties undertakes to take into account the recommendations of the other Party regarding the subject matter of this Agreement. The Parties undertake to immediately inform each other about the difficulties that impede the fulfillment of the terms of this Agreement, in order to take the necessary measures in a timely manner.'
+           , 'Footer of contract', 'Details of the parties to the agreement',
+        false, 'Contract field', '2023-24-01 15:35:18', 1, 1);
