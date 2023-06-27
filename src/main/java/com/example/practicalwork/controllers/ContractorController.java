57d7912d82b1ffcad7fa6b8922d76e82ccfda996
Ильнур <ilnurka_16@mail.ru>
2023-06-27 21:55:26 +0300

    cosntrComm1

--- src/main/java/com/example/practicalwork/controllers/ContractorController.java
+++ src/main/java/com/example/practicalwork/controllers/ContractorController.java
@@ -2,70 +2,155 @@ package com.example.practicalwork.controllers;
 
 import com.example.practicalwork.DTO.ContractorDTO;
 import com.example.practicalwork.models.Contractor;
+import com.example.practicalwork.models.Document;
 import com.example.practicalwork.services.ContractorService;
+import com.example.practicalwork.utils.BindingResultHandler;
+import com.example.practicalwork.utils.contractor.ContrListIsEmptyException;
+import com.example.practicalwork.utils.contractor.ContrNotCreatedException;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Controller
+@RestController
 @RequestMapping("/api/contractor")
 public class ContractorController {
 
     private final ContractorService contractorService;
     private final ContractorDTO contractorDTO;
     private final ModelMapper modelMapper;
+    private final BindingResultHandler bindingResultHandler;
 
 
-    public ContractorController(ContractorService contractorService, ContractorDTO contractorDTO, ModelMapper modelMapper) {
+    public ContractorController(ContractorService contractorService, ContractorDTO contractorDTO, ModelMapper modelMapper, BindingResultHandler bindingResultHandler) {
         this.contractorService = contractorService;
         this.contractorDTO = contractorDTO;
         this.modelMapper = modelMapper;
+        this.bindingResultHandler = bindingResultHandler;
     }
 
-//    @GetMapping()
-//    public List<ContractorDTO> getContractors () {
-//        return contractorService.allContractors().stream()
-//                .map(this::convertToContractor)
-//                .collect(Collectors.toList());
-//    }
+    @GetMapping("/all")
+    public List<ContractorDTO> getContractors () {
+        return contractorService.allContractors().stream()
+                .map(this::convertToContractor)
+                .collect(Collectors.toList());
+    }
 
-//    @GetMapping
-//    public Contractor getContractorByDocument (Contractor contractor, @RequestParam List<Document> document) {
-//        return contractorService.findContractorByDocuments(document);
-//    }
-//
-//    //УТОЧНИТЬ НИЖЕ
-//    @GetMapping
-//    public Contractor getContractorByFilter (Contractor contractor, @RequestParam String firstName, @RequestParam String patronymic,
-//                                             @RequestParam String lastName, @RequestParam String country, @RequestParam Credential credential) {
-//        return contractorService.findContractorByFilter(contractor);
-//    }
+    @GetMapping("/{id}")
+    public ContractorDTO getById (@PathVariable("id") Long id) {
+        return convertToContractor(contractorService.findContractor(id));
+    }
+
+    @GetMapping("/current")
+    public List<ContractorDTO> getCurrent() {
+        List<ContractorDTO> contr = contractorService.findCurrent()
+                .stream().map(this::convertToContractor)
+                .toList();
+        if ((contr.isEmpty())) {
+            throw new ContrListIsEmptyException();
+        }
+        return contr;
+    }
+
+    @GetMapping("/byDoc")
+    public List<ContractorDTO> getContractorByDocument (@RequestParam(value = "document", required = false) String documentId){
+        List<ContractorDTO> list = contractorService.allContractors().stream()
+                .filter(x -> {
+                    if (documentId != null) {
+                        return x.getDocuments().stream().anyMatch(c -> c.getId().equals(Long.parseLong(documentId)));
+                    } else return true;
+                })
+                .map(this::convertToContractor)
+                .toList();
+        return list;
+    }
+
+    @GetMapping("/filter")
+    public List<ContractorDTO> getContractorByFilter (@RequestParam(value = "firstName", required = false) String firstName,
+                                                      @RequestParam(value = "patronymic", required = false) String patronymic,
+                                                      @RequestParam(value = "lastName", required = false) String lastName,
+                                                      @RequestParam(value = "country", required = false) String country,
+                                                      @RequestParam(value = "credential", required = false) String credentialId,
+                                                      @RequestParam(value = "current", required = false, defaultValue = "true")
+                                                      String current) {
+        List<ContractorDTO> list = contractorService.allContractors().stream()
+                .filter(x -> {
+                    if (Boolean.parseBoolean(current)) {
+                        return !x.isRemoved();
+                    } else return true;
+                })
+                .filter(x -> {
+                    if (firstName != null) {
+                        return x.getFirstName().contains(firstName);
+                    } else return true;
+                })
+                .filter(x -> {
+                    if (patronymic != null) {
+                        return x.getPatronymic().contains(patronymic);
+                    } else return true;
+                })
+                .filter(x -> {
+                    if (lastName != null) {
+                        return x.getLastName().contains(lastName);
+                    } else return true;
+                })
+                .filter(x -> {
+                    if (country != null) {
+                        return x.getCountry().contains(country);
+                    } else return true;
+                })
+                .filter(x -> {
+                    if (credentialId != null) {
+                        return x.getCredential().getId().equals(Long.parseLong(credentialId));
+                    } else return true;
+                })
+                .map(this::convertToContractor)
+                .toList();
+        if (list.isEmpty()) {
+            throw new ContrListIsEmptyException();
+        }
+        return list;
+    }
 
     private ContractorDTO convertToContractor (Contractor contractor) {
         return modelMapper.map(contractor, ContractorDTO.class);
     }
-
-    @PostMapping
-    public ResponseEntity<ContractorDTO> createdContractor (@RequestBody @Valid Contractor contractor) {
-       Contractor newContractor = contractorService.saveCreatedContractor(contractor);
-        return ResponseEntity.accepted().body(convertToContractor(newContractor));
+    public Contractor convertToContractorDTO (ContractorDTO contractorDTO) {
+        return modelMapper.map(contractorDTO, Contractor.class);
     }
 
-//    @PostMapping("/{id}")
-//    public ResponseEntity<ContractorDTO> updateContractor (@RequestBody @Valid Contractor updateContractor, @PathVariable Long id) {
-//        Contractor newContractor = contractorService.updateContractor(id, updateContractor);
-//        return ResponseEntity.accepted().body(convertToContractor(newContractor));
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Response> deleteContractor (@PathVariable("id") Long id) {
-//        contractorService.deleteContractor(id);
-//        return ResponseEntity.accepted().body(new Response());
-//    }
+    @PostMapping("/new")
+    public ResponseEntity<HttpStatus> createdContractor (@RequestBody @Valid ContractorDTO creatContractor,
+                                                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String sad = bindingResultHandler.createErrorMessage(bindingResult);
+            throw new ContrNotCreatedException(sad);
+        }
+        contractorService.saveCreatedContractor(convertToContractorDTO(creatContractor));
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<HttpStatus> updateContractor (@RequestBody @Valid ContractorDTO updateContractor,
+                                                           BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String das = bindingResultHandler.createErrorMessage(bindingResult);
+            throw new ContrNotCreatedException(das);
+        }
+        contractorService.updateContractor(convertToContractorDTO(updateContractor));
+        return ResponseEntity.ok(HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteContractor (@PathVariable("id") Long id) {
+        contractorService.deleteContractor(id);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/example/practicalwork/services/ContractorService.java
+++ src/main/java/com/example/practicalwork/services/ContractorService.java
@@ -2,14 +2,18 @@ package com.example.practicalwork.services;
 
 
 import com.example.practicalwork.models.Contractor;
-import com.example.practicalwork.models.Document;
 import com.example.practicalwork.repositories.ContractorRepository;
+import com.example.practicalwork.utils.contractor.ContrIsDeletedException;
+import com.example.practicalwork.utils.contractor.ContrNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@Transactional(readOnly = true)
 public class ContractorService {
     private final ContractorRepository contractorRepository;
 
@@ -18,62 +22,50 @@ public class ContractorService {
         this.contractorRepository = contractorRepository;
     }
 
-//    public List <Contractor> allContractors () {
-//        return contractorRepository.findAllContractors();
-//    }
-
-//    public Contractor findContractor (Long id) {
-//        return contractorRepository.findContractorById(id);
-//    }
-
-//    public Contractor updateContractor (Long id, Contractor updateContractor) {
-////            Contractor contractor = findContractor(id);
-//            contractor.setFirstName(updateContractor.getFirstName());
-//            contractor.setPatronymic(updateContractor.getPatronymic());
-//            contractor.setLastName(updateContractor.getLastName());
-//            contractor.setCountry(updateContractor.getCountry());
-//            contractor.setPhone(updateContractor.getPhone());
-//            contractor.setEmail(updateContractor.getEmail());
-//            contractor.setTelegram(updateContractor.getTelegram());
-//            contractor.setCredential(updateContractor.getCredential());
-//            contractorRepository.save(contractor);
-//            return contractor;
-//    }
-
-//    public Contractor findContractorByDocuments (List<Document> document) {
-//            return contractorRepository.findContractorByDocuments(document);
-//    }
-
-    //УТОЧНИТЬ НИЖЕ
-//    public Contractor findContractorByFilter (Contractor contractor) {
-//        return contractorRepository.findContractorByFilter(contractor.getFirstName(), contractor.getPatronymic(),
-//                contractor.getLastName(), contractor.getCountry(), contractor.getCredential());
-//    }
-
-    public Contractor saveCreatedContractor (Contractor createdContractor) {
-        Contractor contractor = new Contractor();
-        if (createdContractor != null) {
-        contractor.setFirstName(createdContractor.getFirstName());
-        contractor.setPatronymic(createdContractor.getPatronymic());
-        contractor.setLastName(createdContractor.getLastName());
-        contractor.setCountry(createdContractor.getCountry());
-        contractor.setPhone(createdContractor.getPhone());
-        contractor.setEmail(createdContractor.getEmail());
-        contractor.setTelegram(createdContractor.getTelegram());
-        contractor.setCredential(createdContractor.getCredential());
-        contractorRepository.save(contractor);
-        return contractor;
-    } else {
-            return null;
-        }
+    public List<Contractor> allContractors() {
+        return contractorRepository.findAll();
     }
 
-//    public Contractor deleteContractor (Long id) {
-//        Contractor contractor = findContractor(id);
-//            contractor.setRemoved(true);
-//            contractor.setDocuments(null);
-//            contractorRepository.save(contractor);
-//            return contractor;
-//        }
+    public List<Contractor> findCurrent() {
+        return contractorRepository.findAll().stream()
+                .filter((x -> !x.isRemoved()))
+                .toList();
+    }
+
+    public Contractor findContractor(Long id) {
+        Optional<Contractor> conOpt = contractorRepository.findById(id);
+        return conOpt.orElseThrow(ContrNotFoundException::new);
+    }
+
+    @Transactional
+    public void updateContractor(Contractor updateContractor) {
+        Contractor contractor = findContractor(updateContractor.getId());
+        contractor.setFirstName(updateContractor.getFirstName());
+        contractor.setPatronymic(updateContractor.getPatronymic());
+        contractor.setLastName(updateContractor.getLastName());
+        contractor.setCountry(updateContractor.getCountry());
+        contractor.setPhone(updateContractor.getPhone());
+        contractor.setEmail(updateContractor.getEmail());
+        contractor.setTelegram(updateContractor.getTelegram());
+        contractor.setCredential(updateContractor.getCredential());
+        contractorRepository.save(contractor);
+    }
+
+    @Transactional
+    public void saveCreatedContractor(Contractor createdContractor) {
+        contractorRepository.save(createdContractor);
+    }
+
+    @Transactional
+    public void deleteContractor(Long id) {
+        Contractor contractor = findContractor(id);
+        if (contractor.isRemoved()) {
+            throw new ContrIsDeletedException();
+        } else {
+            contractor.setRemoved(true);
+            contractor.setDocuments(null);
+            contractorRepository.save(contractor);
+        }
+    }
 
 }
